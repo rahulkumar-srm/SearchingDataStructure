6e7c0def8e6d14e7bb2b4cc72d1aa478a19d0c2b
Rahul Kumar <rahul.kumar14@stryker.com>
2021-06-03 21:25:19 +0530

    Add project files.

---
+++ SearchingDataStructure/Helper/Searching.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchingDataStructure.Helper
+{
+    internal class Searching
+    {
+        internal static void IterativeBinarySearch()
+        {
+            Console.WriteLine("Enter the numbers in sorted order(comma separated)"); // 2,5,9,15,25,33,56,64
+            int[] items = Console.ReadLine().Split(',').Select(x => Convert.ToInt32(x.Trim())).ToArray();
+
+            Console.WriteLine("Enter the element to be searched");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int low = 0;
+            int high = items.Length - 1;
+
+            while(low <= high)
+            {
+                int mid = (low + high) / 2;
+
+                if(items[mid] == num)
+                {
+                    Console.WriteLine($"Element {num} found at index : {mid}");
+                    return;
+                }
+                else if(num > items[mid])
+                {
+                    low = mid + 1;                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+
+            Console.WriteLine("Element not found");
+        }
+
+        internal static void RecursiveBinarySearch(int[] items, int num, int low, int high)
+        {
+            if(low > high)
+            {
+                Console.WriteLine("Element not found");
+                return;
+            }
+
+            int mid = (low + high) / 2;
+
+            if(items[mid] == num)
+            {
+                Console.WriteLine($"Element {num} found at index : {mid}");
+            }
+            else if (num > items[mid])
+            {
+                RecursiveBinarySearch(items, num, mid + 1, high);
+            }
+            else
+            {
+                RecursiveBinarySearch(items, num, low, mid - 1);
+            }
+        }
+    }
+}
---
+++ SearchingDataStructure/Program.cs
@@ -0,0 +1,53 @@
+﻿using SearchingDataStructure.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchingDataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine
+                    ("Please select an option" +
+                        Environment.NewLine + "1. Iterative Binary Search" +
+                        Environment.NewLine + "2. Recursive Binary Search" +
+                        Environment.NewLine + "0. Exit"
+                    );
+
+                if (!int.TryParse(Console.ReadLine(), out int i))
+                {
+                    Console.WriteLine(Environment.NewLine + "Input format is not valid. Please try again." + Environment.NewLine);
+                }
+
+                if (i == 0)
+                {
+                    Environment.Exit(0);
+                }
+                else if (i == 1)
+                {
+                    Searching.IterativeBinarySearch();
+                }
+                else if (i == 2)
+                {
+                    Console.WriteLine("Enter the numbers in sorted order(comma separated)"); // 2,5,9,15,25,33,56,64
+                    int[] items = Console.ReadLine().Split(',').Select(x => Convert.ToInt32(x.Trim())).ToArray();
+
+                    Console.WriteLine("Enter the element to be searched");
+                    int num = Convert.ToInt32(Console.ReadLine());
+
+                    Searching.RecursiveBinarySearch(items, num, 0, items.Length - 1);
+                }
+                else
+                {
+                    Console.WriteLine("Please select a valid option.");
+                }
+            }
+        }
+    }
+}
